11fb8e63efbe447b76e4c41355fa914083f45500
Lorenzo Vee <134215240+AmazedAce@users.noreply.github.com>
2024-05-04 21:34:42 -0500

    Add files via upload

---
+++ Homework 1.py
@@ -0,0 +1,88 @@
+#Task 1: Code Correction
+
+# weather = "sunny"
+
+# if weather == "sunny":
+#     print("Wear sunglasses!")
+# else:
+#     print("Take an umbrella!")
+
+#Task 2: Your Mood Today
+
+# print("How do you feel today?")
+
+#  if "happy":
+#     print("That's great to hear!")
+# else:
+#     print("I hope your day gets better!")
+
+
+
+# Ask the user how they feel today
+# user_feeling = input("How do you feel today? (happy or sad): ")
+
+# # Check the user's response and print the appropriate message
+# if user_feeling == "happy":
+#     print("That's great to hear!")
+# elif user_feeling == "sad":
+#     print("I hope your day gets better!")
+
+# 2. Python Naming Convention Practice
+
+
+# Task 1: Code Correction
+
+
+# pi_value = 3.14
+# user_age = 25
+# user_location = "New York"
+# max_limit = 1000
+
+# 3. Python Data Types and type() Function
+
+# Task 1: Code Correction
+
+# variable_a = "Hello, World!"
+# variable_b = 23
+# variable_c = 3.14
+# variable_d = True
+
+# print(type(variable_a))
+# print(type(variable_b))
+# print(type(variable_c))
+# print(type(variable_d))
+
+# 4. Arithmetic Operations in Daily Life
+
+# Task 2: Bank Interest
+
+# fruits = {"apple": 1.79,"banana": 0.39,"orange": 0.79}
+# total_cost = sum(fruits.value())
+# print(total_cost)
+
+
+# 5. Understanding Assignments and Comparisons
+
+# Task 1: Value Swapping
+
+# initial_rate = 10700
+# interest_rate = 7
+
+# total_amount = initial_rate * (1 + interest_rate / 100)
+
+#print("after one year. the total is:", total_amount)
+
+
+# 6. Exploring Logical Operations and Precedence
+
+# Task 1: Validating Calculations
+# normal_calculation = 3 + 4 * 2
+# with_parentheses = (3 + 4) * 2
+
+# Task 2: Mix and Match
+# mix_match_expression = 3 + 4 > 5 or 2 + 2 == 4
+
+# Output results
+# print("Task 1 - Normal Calculation:", normal_calculation)
+# print("Task 1 - With Parentheses:", with_parentheses)
+# print("Task 2 - Mix and Match Result:", mix_match_expression)
